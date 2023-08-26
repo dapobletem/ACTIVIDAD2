11186633d5f9097880fb4a926d954785b7aa3793
Vina del Mar <vina@duoc.acad>
2023-08-26 09:38:55 -0400

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,14 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
+  
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: '**',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  }
 ];
